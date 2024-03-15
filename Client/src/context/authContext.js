b934a8f34966832b0dfb4e5dd33349f6cc4d6bc6
MAXIMILIAN <maxibahrami@gmail.com>
2024-03-16 04:10:43 +0500

    Update authContext.js

--- Client/src/context/authContext.js
+++ Client/src/context/authContext.js
@@ -8,8 +8,10 @@ export const AuthContextProvider = ({children})=>{
 
   const login = async (inputs) => {
     try {
-        axios.defaults.withCredentials = true;
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, inputs);
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, inputs,{
+            withCredentials: true,
+          }
+);
       if (res.data) {
         // Assuming the response contains user data
         setCurrentUser(res.data);
