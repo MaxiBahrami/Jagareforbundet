9e4a2c8e5356196632eadaa4845187977f2010ac
MAXIMILIAN <maxibahrami@gmail.com>
2024-03-15 02:31:00 +0100

    Delete Api directory

--- Api/data-fetching/urlConfig.js
+++
@@ -1,7 +0,0 @@
-
-export const urlConfig = {
-  riksUrl : 'https://jagareforbundet.se/sok/?q=riks&page=1&size=10&sort=date&areas=0&types=38',
-  lansUrl : 'https://jagareforbundet.se/sok/?q=stockholms&page=1&size=10&sort=date&areas=1145&types=38',
-  // lokaltUrl : 'https://jagareforbundet.se/sok/?q=riks&page=1&size=10&sort=date&areas=0&types=38',
-  aktiviteterUrl : 'https://jagareforbundet.se/sok/?q=aktiviteter&page=1&size=10&sort=date&areas=0&types=38',
-}
