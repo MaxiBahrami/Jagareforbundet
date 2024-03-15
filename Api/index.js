a60b406860a186820bd0112c96ae07c2fa108ccb
MAXIMILIAN <maxibahrami@gmail.com>
2024-03-16 02:23:41 +0500

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 }))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://jaktharry-app.netlify.app'); // Allow all origins (replace with specific origins if needed)
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://jaktharry-app.netlify.app', 'https://jaktharry-app.vercel.app'); // Allow all origins (replace with specific origins if needed)
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
